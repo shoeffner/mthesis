7e02ea9a09419c894eb7e475a0d5dcbea7c5628d
Sebastian Höffner <info@sebastian-hoeffner.de>
2018-01-05 17:10:00 +0100

    Using unique_ptr instead of old new/delete.

--- assets/examples/gaze_simple/gaze_simple.cpp
+++ assets/examples/gaze_simple/gaze_simple.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <memory>
 #include <utility>
 
 #include "gaze/gaze.h"
 
 int main(const int, const char** const) {
-  gaze::GazeTracker* tracker = new gaze::GazeTracker("subject");
+  std::unique_ptr<gaze::GazeTracker> tracker(new gaze::GazeTracker("1"));
   for (int i = 0; i < 20; ++i) {
     std::pair<int, int> point = tracker->get_current_gaze_point();
     std::cout << point.first << ", " << point.second << std::endl;
   }
-  delete tracker;
 }
