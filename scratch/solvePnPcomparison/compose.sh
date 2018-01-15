121490caec0743af0fd634dd1dca74b565afadba
Sebastian Höffner <info@sebastian-hoeffner.de>
2018-01-15 19:38:33 +0100

    Adding a comparison for solvePnP strategies.

---
+++ scratch/solvePnPcomparison/compose.sh
@@ -0,0 +1,6 @@
+convert +append solvePnP*_0000.png solvePnP_0000.png
+convert +append solvePnP*_0025.png solvePnP_0025.png
+convert +append solvePnP*_0031.png solvePnP_0031.png
+convert +append solvePnP*_0044.png solvePnP_0044.png
+convert -append solvePnP_*.png ../../assets/images/solvePnPcomparison.png
+rm solvePnP_*.png
