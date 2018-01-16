8c0128ece92319cefb4259efc0f5584195bae583
Sebastian Höffner <info@sebastian-hoeffner.de>
2018-01-16 17:03:24 +0100

    Using montage instead of append etc to add nice boundaries.

--- scratch/solvePnPcomparison/compose.sh
+++ scratch/solvePnPcomparison/compose.sh
@@ -1,6 +1,2 @@
-convert +append solvePnP*_0000.png solvePnP_0000.png
-convert +append solvePnP*_0025.png solvePnP_0025.png
-convert +append solvePnP*_0031.png solvePnP_0031.png
-convert +append solvePnP*_0044.png solvePnP_0044.png
-convert -append solvePnP_*.png ../../assets/images/solvePnPcomparison.png
-rm solvePnP_*.png
+montage solvePnP*_0000.png solvePnP*_0025.png solvePnP*_0031.png solvePnP*_0044.png \
+        -tile 2x -geometry "300x200>+3+3" ../../assets/images/solvePnPcomparison.png
