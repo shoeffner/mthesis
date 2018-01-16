84f3aa039e35470536264f3df5caa1c12feaba7f
Sebastian Höffner <info@sebastian-hoeffner.de>
2018-01-16 17:05:12 +0100

    Adding images to compare the different pupil localization implementations.

---
+++ scratch/pupildetection/README.md
@@ -0,0 +1,10 @@
+# `pupil_detection_comparison.png`
+
+![](../../assets/images/pupil_detection_comparison.png)
+
+The individual groups of four images show eyeLike's results in the top row and Gaze's results in the bottom row, the eyes are ordered as they are in the original images. The position of each group matches the position of the original image in the figure below.
+
+
+# `pupil_detection_faces.png`
+
+![](../../assets/images/pupil_detection_faces.png)
---
+++ scratch/pupildetection/compose.sh
@@ -0,0 +1,15 @@
+imlist=`ls eye*b.png | sed 's/eyeLike//' | sed 's/b.png//'`
+
+# montage temp patches
+for i in $imlist; do
+    montage eyeLike${i}r.png eyeLike${i}l.png pupil${i}r.png pupil${i}l.png \
+            -geometry 50x50+3+3 mon${i}.png
+done
+
+# combine patches
+montage mon*.png -tile x6 ../../assets/images/pupil_detection_comparison.png
+rm mon*.png
+
+# combine original images
+montage `for i in $imlist; do echo ../pexels_face_images/photos/${i}.jpeg; done` \
+        -tile x6 -geometry "120x120>+6+6" ../../assets/images/pupil_detetction_faces.png
